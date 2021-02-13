1ad8937f54270cbeab5d6073485f7ff250115786
Matthew Wilkins, PhD <mrwilkins06@gmail.com>
2021-02-13 00:54:19 -0600

    added environment() declaration to data to try to solve the problem of not running on a new machine when package is pulled from github

--- R/gpColors.R
+++ R/gpColors.R
@@ -3,17 +3,22 @@
 #' output a vector of hex codes, given named colors; list=TRUE lists all options
 #'
 #' @param colorNames a vector of quoted named colors (partial name matching is allowed)
-#' @param list T/F, do you want to list the options?
+#' @param List T/F, do you want to list the options?
 #' @return a vector of hex codes for the requested colors
 #' @export
-gpColors<-function(colorNames,list=FALSE){
+gpColors<-function(colorNames=NULL,List=FALSE){
   gpPal=NULL
-  utils::data(gpPal)
-  allPal.0<-do.call(dplyr::bind_rows,gpPal)
+  utils::data(gpPal,package="GPpub",envir = environment())
+  allPal.0<-do.call(rbind,gpPal)
   allPal<-subset(allPal.0,!duplicated(allPal.0$name))
-  if(list==TRUE){cat("GP PALETTE OPTIONS",paste0(rep("-",20),collapse=""),"\n",paste(allPal[,1],collapse=", "))}
-  indx=charmatch(colorNames,allPal$name)
-  colVec<-allPal$hex[indx]
-  names(colVec)<-allPal$name[indx]
-  return(colVec)
+  if(List==TRUE|is.null(colorNames)){
+    cat("GP PALETTE OPTIONS",paste0(rep("-",20),collapse=""),
+        "\n",paste(allPal[,1],collapse=", "))
+    return()
+    }else{
+    indx=charmatch(colorNames,allPal$name)
+    colVec<-allPal$hex[indx]
+    names(colVec)<-allPal$name[indx]
+    return(colVec)}
+
 }
--- R/show.gpShapes.R
+++ R/show.gpShapes.R
@@ -5,6 +5,6 @@
 #'
 show.gpShapes<-function(){
    gpShps=NULL
-  utils::data(gpShps)
+  utils::data(gpShps,package="GPpub",envir = environment())
 plot(0:length(gpShps),0:length(gpShps),pch=gpShps,cex=2,xlim=c(-1,length(gpShps)+1),ylim=c(-1,length(gpShps)+1))
 }
