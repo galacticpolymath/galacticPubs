f3a0df64b30adbfda476536a707fa8c028394bbd
Matthew Wilkins, PhD <>
2021-03-08 16:12:08 -0600

    added font.col parameter to ggGalactic

--- R/ggGalactic.R
+++ R/ggGalactic.R
@@ -8,11 +8,12 @@
 #' @param regular.wt font weight for regular font style
 #' @param bold.wt font weight for bold text
 #' @param font.cex a simple multiplier for scaling all text
+#' @param font.col color of all axis label and title text
 #' @param plot.margin easy access to ggplot margins
 #' @export
 
 
-ggGalactic<-function(font="Montserrat",regular.wt=400,bold.wt=700,font.cex=1,plot.margin=ggplot2::margin(t=10,r=10,b=10,l=10)){
+ggGalactic<-function(font="Montserrat",regular.wt=400,bold.wt=700,font.cex=1,font.col="#363636",plot.margin=ggplot2::margin(t=10,r=10,b=10,l=10)){
   gpPal=NULL
   utils::data(gpPal)
   showtext::showtext_auto()
@@ -21,14 +22,14 @@ ggGalactic<-function(font="Montserrat",regular.wt=400,bold.wt=700,font.cex=1,plo
 ggplot2::theme_linedraw()+ggplot2::theme(
     text=ggplot2::element_text(family=font),
     plot.margin=plot.margin,
-    plot.title=ggplot2::element_text(family=font,size=30*font.cex,face="bold",color=gpPal[[1]]$hex[6]),
+    plot.title=ggplot2::element_text(family=font,size=30*font.cex,face="bold",color=font.col),
     plot.subtitle=ggplot2::element_text(family=font,size=22*font.cex,color=gpPal[[1]]$hex[5]),
-    axis.title=ggplot2::element_text(family=font,size=28*font.cex,face="bold",color=gpPal[[1]]$hex[6]),
-    axis.text=ggplot2::element_text(family=font,size=18*font.cex,color=gpPal[[1]]$hex[6]),
+    axis.title=ggplot2::element_text(family=font,size=28*font.cex,face="bold",color=font.col),
+    axis.text=ggplot2::element_text(family=font,size=18*font.cex,color=font.col),
     axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 10, r = 0, b = 0, l = 0)),
     axis.title.y = ggplot2::element_text(margin = ggplot2::margin(t = 20, r = 10, b = 0, l = 0)),
-    legend.text=ggplot2::element_text(family=font,color=gpPal[[1]]$hex[6],size=18*font.cex),
-    legend.title=ggplot2::element_text(family=font,color=gpPal[[1]]$hex[6],face="bold",size=18*font.cex),
+    legend.text=ggplot2::element_text(family=font,color=font.col,size=18*font.cex),
+    legend.title=ggplot2::element_text(family=font,color=font.col,face="bold",size=18*font.cex),
     legend.position = "right", legend.text.align = 0, legend.background =ggplot2::element_blank()
   )
 }
