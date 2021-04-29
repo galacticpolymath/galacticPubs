6f4585b40767a721354194b723d041910990e06a
Matt Wilkins <matt@galacticpolymath.com>
2021-04-29 16:14:00 -0500

    compileProcedure makes sensible JSON output

--- R/compileProcedure.R
+++ R/compileProcedure.R
@@ -6,12 +6,12 @@
 #' markdown; e.g. "\{vid1\}" will be replaced with a markdown link to the video in the links spreadsheet multimedia tab
 #' that has order=1
 #' @param destFolder where you want to save the folder; by default in the "meta/JSON/" folder
-#' @param fileName output file name; default= "processedProcedure.json"
+#' @param outputFileName output file name; default= "processedProcedure.json"
 #' @return tibble of the compiled standards data; a JSON is saved to meta/JSON/processedProcedure.json
 #' @importFrom rlang .data
 #' @export
 #'
-compileProcedure <- function(procedureFile="meta/procedure.xlsx",linksFile="meta/teaching-resource-links.xlsx",destFolder="meta/JSON/" ,fileName="processedProcedure.json"){
+compileProcedure <- function(procedureFile="meta/procedure.xlsx",linksFile="meta/teaching-resource-links.xlsx",destFolder="meta/JSON/" ,outputFileName="processedProcedure.json"){
 
    .=NULL #to avoid errors with dplyr syntax
 
@@ -20,7 +20,50 @@ compileProcedure <- function(procedureFile="meta/procedure.xlsx",linksFile="meta
   #read in Part titles and lesson + Part prefaces
   procTitles<-xlsx::read.xlsx2(procedureFile,sheetName="NamesAndNotes")%>% dplyr::tibble() %>% dplyr::filter(.data$Part!="") %>% dplyr::select(-dplyr::starts_with("X."))
 
+  #Basic test
+  nPartsTest<-length(unique(proc$Part))==length(unique(procTitles$Part))
+  message("TEST: Num. Parts in 'NamesAndNotes' = 'Procedure'?   ** ",ifelse(nPartsTest,"PASS","FAIL")," **")
+
   #Parse all the text columns to expand {vidN} notation into full video links
   proc[,c("StepQuickDescription","StepDetails","VariantNotes","TeachingTips")]<-apply(proc[,c("StepQuickDescription","StepDetails","VariantNotes","TeachingTips")],2,function(x) parseGPmarkdown(x))
 
-   }
+  #Let's make a list that we'll convert to JSON
+  out<-list()
+  for(i in 1:length(unique(procTitles$Part))){
+    part <- i
+    title <- procTitles$PartTitle[i]
+    dur <- proc$PartDur[i]
+    preface<-procTitles$PartPreface[i]
+    chunks<-lapply(unique(subset(proc,Part==i)$Chunk),function(chunk_i){
+              d<-subset(proc,Part==i&Chunk==chunk_i)
+              title<-d$ChunkTitle[1]
+              dur<-d$ChunkDur[1]
+              steps<-d %>% dplyr::select("Step","StepTitle","StepQuickDescription","StepDetails","Vocab","VariantNotes",
+                                         "TeachingTips")
+              list(title=title,dur=dur,steps=list(steps))
+              })
+    out[[i]]<-list(part=part,title=title,dur=dur,preface=preface,chunks=chunks)
+  }
+
+# Make json structured output ----------------------------------------------
+
+# create directory if necessary & prep output filename --------------------
+dir.create(destFolder,showWarnings=FALSE,recursive=T)
+outFile<-fs::path(destFolder,paste0(sub(pattern="(.*?)\\..*$",replacement="\\1",x=basename(outputFileName))),ext="json")
+
+
+# Write JSON for GP Simple Lesson Plan -----------------------------------
+compiled_json<-jsonlite::toJSON(out,pretty=TRUE,auto_unbox = TRUE)
+con<-file(outFile)
+writeLines(compiled_json,con)
+close(con)
+
+# printToScreenTable<-cbind(ack[,c("Role","Name","Title")],OtherInfo="BlahBlah")
+
+# return compiled output --------------------------------------------------
+message("Procedures compiled:")
+# print(printToScreenTable)
+message("JSON file saved\n@ ",outFile)
+
+
+}
