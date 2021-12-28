45251fcdd7782492d244bf71498538c76f648146
Matt Wilkins <matt@galacticpolymath.com>
2021-12-28 15:17:47 -0600

    rework learningEpaulette to be interactive

--- R/learningEpaulette.R
+++ R/learningEpaulette.R
@@ -3,6 +3,8 @@
 #' Create a Galactic Polymath Learning Epaulette which is a special kind of mosaic plot showing proportion of lesson by subject
 #' @param compiledAlignment the output of \code{\link{compileStandards}}
 #' @param targetSubj which subject`(`s`)` is `(`are`)` the focus of the lesson? opts= "math","ela","science","social studies"
+#' @param labelPadding for horizontal epaulette, the amount of padding between label text and bottom as an arbitrary ratio; default=1
+#' @param randomSeed random number for getting slightly different (but repeatable) repelled text labels
 #' @param saveFile T/F, save file or just print to screen?
 #' @param destFolder where do you want to save the folder; by default in the "assets/learningPlots" folder, 1 level up from the working directory
 #' @param fileName expects "somefilename" for ggsave output image file
@@ -20,7 +22,7 @@
 #########################################
 ### GP Learning Mosaic Plot/Epaulet graphic
 
-learningEpaulette<-function(compiledAlignment,targetSubj=NULL,saveFile=TRUE,destFolder="assets/learning-plots/",fileName="GP-Learning-Epaulette",WD=getwd(),font_size=19,thickness=0.2,width=11,height=1.6,dpi=200,...){
+learningEpaulette<-function(compiledAlignment,targetSubj=NULL,labelPadding=1,randomSeed=101,saveFile=TRUE,destFolder="assets/learning-plots/",fileName="GP-Learning-Epaulette",WD=getwd(),font_size=19,thickness=0.2,width=11,height=1.6,dpi=200,...){
 
   #if WD supplied, append it to destFolder
   if(!identical(WD,getwd())){destFolder<-paste0(WD,destFolder)}
@@ -61,7 +63,7 @@ proportions=a_combined  %>% dplyr::group_by(.data$subject)%>% dplyr::summarise(p
 
 # dummy proportions
 
-# proportions$proportion<-c(.3,.2,.1,0.4)
+proportions$proportion<-c(.7,.1,.1,0.1)
 
 xlabels<-sapply(proportions$proportion,scales::percent) %>% dplyr::as_tibble()
 xlabels$x.prop=(proportions$proportion)
@@ -118,19 +120,34 @@ epaulette<-
   ggplot2::theme_void()+
     ggplot2::theme(plot.background=ggplot2::element_blank(),panel.background = ggplot2::element_blank())
 
+
 subject_labels<-subset(xlabels,xlabels$x.prop>0) %>% ggplot2::ggplot()+
   galacticEdTools::theme_galactic(font="Kanit")+
   ggrepel::geom_text_repel(ggplot2::aes_string(x="x",y=.2,label="lab",fontface="fontface",
                                                segment.size="stroke"),
-                           family="Kanit",size=font_size,show.legend = FALSE,direction="y",col=gpColors("galactic black"),force=5)+
+                           seed=randomSeed,
+                           family="Kanit",size=font_size,show.legend = FALSE,direction="y",col=gpColors("galactic black"),force=10,
+                           box.padding=0.25,point.padding=0.25)+
+  #scale y to pad text according to user input
   ggplot2::scale_y_continuous(limits=c(0,0.2),expand=ggplot2::expansion(c(0,0)))+
   ggplot2::scale_x_continuous(limits = c(0,1),expand=ggplot2::expansion(c(0,0)))+
    ggplot2::theme_void()+
     ggplot2::theme(plot.background=ggplot2::element_blank(),panel.background = ggplot2::element_blank())
 
+###
+# HORIZONTAL EPAULETTE + LABELS
+(G <- epaulette / subject_labels  + patchwork::plot_layout(ncol = 1, heights = c(0.3, 0.7),widths=c(1,1)) &
+    ggplot2::theme(
+    plot.background = ggplot2::element_blank(),
+    panel.background = ggplot2::element_blank()
+    )
+  )
 
-(G <- epaulette+subject_labels+patchwork::plot_layout(ncol=1,heights=c(0.3,0.7)) &
-    ggplot2::theme(plot.background=ggplot2::element_blank(),panel.background = ggplot2::element_blank()))
+browser()
+
+
+
+
 
 epaulette_vert<-  ggplot2::ggplot(rectangles)+
   ggplot2::geom_rect(ggplot2::aes_string(xmin="xmin",xmax="xmax",ymin="ymin",ymax="ymax",fill="subject"),size=1.2,show.legend = F)+
@@ -166,6 +183,8 @@ subject_labels_vert <- subset(xlabels,xlabels$x.prop>0) %>% ggplot2::ggplot()+
   ggplot2::scale_y_continuous(limits = c(0,1),expand=ggplot2::expansion(c(0.001,0.001)))+
    ggplot2::theme_void()
 
+###
+# VERTICAL EPAULETTE + LABELS
 (G_vert <- epaulette_vert+subject_labels_vert+patchwork::plot_layout(ncol=2,widths=c(0.2,0.8)) &
     ggplot2::theme(plot.background=ggplot2::element_blank(),panel.background = ggplot2::element_blank()))
 #make a vertical version of this
@@ -175,21 +194,24 @@ subject_labels_vert <- subset(xlabels,xlabels$x.prop>0) %>% ggplot2::ggplot()+
 dir.create(destFolder,showWarnings=FALSE, recursive=TRUE)
 
 
-givenExt=if(grepl(".",fileName,fixed=TRUE)){gsub(".*\\.(.{3,4}$)","\\1",fileName)}else{NULL} #extract file extension if provided
+givenExt=ifelse(grepl(".",fileName,fixed=TRUE),gsub(".*\\.(.{3,4}$)","\\1",fileName),NA) #extract file extension if provided
 fileOut<-gsub("(^.*)\\..*$","\\1",basename(fileName)) #strip extension and full path from provided fileName
 
 fileOut_vert<-paste0(fileOut,"_vert")
 
-fileOutExt<-ifelse(is.null(givenExt),"png",givenExt) #provide png extension if not provided
+fileOutExt<-ifelse(is.na(givenExt),"png",givenExt) #provide png extension if not provided
 output<-fs::path(destFolder,"/",fileOut,ext=fileOutExt)
 output_vert <- fs::path(destFolder,"/",fileOut_vert,ext=fileOutExt)
 
+
 #save the file
-ggplot2::ggsave(filename=basename(output),plot=G,path=fs::path_dir(output),width=width, height=height,dpi=dpi,bg="transparent",
-                ...)
+ggplot2::ggsave(filename=basename(output),plot=G,path=fs::path_dir(output),width=width,height=height*labelPadding,dpi=dpi,bg="transparent")
+ # ,
+#                 ...)
 #save vertical version
-ggplot2::ggsave(filename=basename(output_vert),plot=G_vert,path=fs::path_dir(output_vert),width=height*1, height=width*.8,dpi=dpi,bg="transparent",
-                ...)
+ggplot2::ggsave(filename=basename(output_vert),plot=G_vert,path=fs::path_dir(output_vert),width=height*1,height=width*.6,dpi=dpi,bg="transparent")
+#,
+ #               ...)
 
 
 #output object if they want to modify further
