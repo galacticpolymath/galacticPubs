fff0ed557614a87cc1f9f6a794da023d7c2018c6
Matt Wilkins <matt@galacticpolymath.com>
2021-12-29 12:53:38 -0600

    big improvements & addition of copyUpdatedFiles helper function to do the handiwork of checking if files are in sync and copying them to a destination folder if the file of interest is out of date

---
+++ R/copyUpdatedFiles.R
@@ -0,0 +1,55 @@
+#' copyUpdatedFiles
+#'
+#' Copy files to destination only if the file is missing or out of date at the destination
+#'
+#' @param paths vector of file paths you want to check and/or copy
+#' @param destFolder directory where you want files to be transferred
+#' @param clear do you want to delete all files in the destination folder before copying? default=FALSE
+#' @param verbose print summary table? default=T
+#' @export
+
+copyUpdatedFiles<-function(paths,destFolder,clear=FALSE,verbose=TRUE){
+  if(clear) {
+    unlink(list.files(destFolder, pattern = "\\.", full.names = TRUE))
+    message("@ Folder cleared: ",destFolder)
+  }
+
+  out<-lapply(paths, function(f) {
+    viable <- file.exists(f)
+    newPath <- fs::path(destFolder, basename(f))
+    # if file exists...
+    if (viable) {
+      #if we didn't delete dest. directory contents...
+      if (!clear) {
+        up_to_date <- suppressWarnings(inSync(f, newPath))
+        if (identical(up_to_date, TRUE)) {
+          status <- "Up-to-Date"
+          toCopy <- FALSE
+        } else{
+          status <- ifelse(is.na(up_to_date), "Added to Destination", "Updated")
+          toCopy <- TRUE
+        }
+      #If we didn't clear the directory, viable path will be copied to dest.
+      }else{
+        status<-"Added to Destination"
+        toCopy<- TRUE
+      }
+    #Nonviable path listed as not found
+    } else{
+      status <- "Not Found"
+      toCopy <- FALSE
+    }
+    if (toCopy) {
+      fs::file_copy(f, newPath, overwrite = TRUE)
+      #make timestamps match bc effing function changes modified date upon *copy*
+      fs::file_touch(newPath,modification_time = file.info(f)$mtime)
+
+    }
+
+    #make summary entry
+    dplyr::tibble(file=f,log=status)
+
+  })
+  if(verbose){print(do.call(dplyr::bind_rows,out))}
+
+}
--- R/inSync.R
+++ R/inSync.R
@@ -1,21 +1,28 @@
 #' inSync
 #'
-#' Checks if 2 files exist, and whether their last modified timestamps match
+#' Checks if 2 files exist, and whether path1 is the same age or newer than path2
 #'
 #' @param path1 path to file of interest
-#' @param path2 path to reference file
+#' @param path2 path to reference file (expected to be at least slightly older)
+#' @param verbose print out table with information? default=FALSE
 #'
 #' @returns T if timestamps match, F if they don't, and NA if one of them doesn't exist
+#' @export
 
-inSync <- function(path1, path2){
-  paths<-c(path1,path2)
-  existance <- sapply(paths, file.exists)
+inSync <- function(path1, path2,verbose=FALSE){
+  path12<-c(path1,path2)
+  existance <- sapply(path12, file.exists)
   if(sum(existance)<2){
-    warning("The following files not found:\n -",paste0(paths[which(existance==FALSE)],collapse="\n- "))
+    warning("The following files not found:\n -",paste0(path12[which(existance==FALSE)],collapse="\n- "))
     NA
   }else{
-    path_info <- lapply(paths,function(x) file.info(x))
-    ifelse(identical(path_info[[1]]$mtime,path_info[[2]]$mtime),TRUE,FALSE)
+    path_info <- lapply(path12,function(x) file.info(x))
+
+    if(verbose){
+      print(do.call(dplyr::bind_rows,path_info)[c(1,4:6)])
+    }
+    #Is path 1 newer than path 2??
+    ifelse(path_info[[1]]$mtime>=path_info[[2]]$mtime,TRUE,FALSE)
   }
 
 }
