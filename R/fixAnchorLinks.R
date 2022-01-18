d960074f8d49e4c39f23f1181311e5984a78915c
Matt Wilkins <matt@galacticpolymath.com>
2022-01-18 17:45:03 -0600

    fix fixAnchorLinks error causing duplication

--- R/fixAnchorLinks.R
+++ R/fixAnchorLinks.R
@@ -7,8 +7,5 @@
 #
 fixAnchorLinks<-function(md){
   pat<- "\\[([^\\[]*?)\\]\\((#.*?)\\)"
-  old_str<-stringr::str_extract_all(md,pat,TRUE)
-  new_str<-gsub(pat,"<AnchorLink href=\"\\2\"> \\1 </AnchorLink>",old_str,perl=TRUE)
-  #replace old with new
-  stringr::str_replace_all(string=md,pattern=pat,new_str)
+  stringr::str_replace_all(string=md,pattern=pat,"<AnchorLink href=\"\\2\"> \\1 </AnchorLink>")
 }
