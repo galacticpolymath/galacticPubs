af58ca2b1f8d7b626633529cd12d6caea4044474
Matt Wilkins <matt@galacticpolymath.com>
2022-04-01 15:48:51 -0500

    galacticPubs::publish() [2022-04-01 15:48:51]  try again

--- R/publish.R
+++ R/publish.R
@@ -18,7 +18,7 @@ publish<- function(commit_msg=NULL,WD=getwd()){
     meta_path<-fs::path(WD,"meta")
 
     # I need to edit both of these files to update First Publication status, etc.
-    current_data<-safe_read_yaml(fs::path(meta_path,"front-matter.yml"))
+    saved_data<-safe_read_yaml(fs::path(meta_path,"front-matter.yml"))
     lesson<-jsonlite::read_json(fs::path(published_path,"LESSON.json"),null="null")
 
     #update publication dates, etc
@@ -26,29 +26,29 @@ publish<- function(commit_msg=NULL,WD=getwd()){
     #Same for id (based on how many lessons currently in catalog)
     time_stamp<-as.character(Sys.time())
 
-    if(is_empty(current_data$FirstPublicationDate)){
-      current_data$FirstPublicationDate<-time_stamp
+    if(is_empty(lesson$FirstPublicationDate)){
+      saved_data$FirstPublicationDate<-time_stamp
       lesson$FirstPublicationDate<-time_stamp
     }
 
     # Assign new id based on what should come next in the catalog
-    if(current_data$id==""){
+    if(lesson$id==""){
       #count how many lessons there are currently on gp-catalog
       current_catalog <- jsonlite::read_json("https://catalog.galacticpolymath.com/index.json")
       next_id<-(sapply(current_catalog, function(x) as.numeric(x$id)) %>% max(na.rm=T) )+1
-      current_data$id<-next_id
+      saved_data$id<-next_id
       lesson$id<-next_id
-      message("Lesson ID assigned: ",current_data$id)
+      message("Lesson ID assigned: ",saved_data$id)
 
     }
 
     #always update LastUpdated timestamp
-    current_data$LastUpdated<-time_stamp
+    saved_data$LastUpdated<-time_stamp
     lesson$LastUpdated<-time_stamp
-    current_data$galacticPubsVer<-as.character
+    saved_data$galacticPubsVer<-as.character
     lesson$galacticPubsVer<-as.character(utils::packageVersion("galacticPubs"))
     #Save time stamp changes
-    yaml::write_yaml(current_data, fs::path(meta_path,"front-matter.yml"))
+    yaml::write_yaml(saved_data, fs::path(meta_path,"front-matter.yml"))
 
     #rewrite it before pushing to cloud
     jsonlite::write_json(lesson,fs::path(published_path,"LESSON.json"),pretty=TRUE,auto_unbox = TRUE,na="null",null="null")
