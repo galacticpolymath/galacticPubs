fda942f147f2a6a813185ded82294fac4f4065f7
Matt Wilkins <matt@galacticpolymath.com>
2022-04-03 19:46:18 -0500

    made a check to prevent accidentally creating a new function def with efx()

--- R/efx.R
+++ R/efx.R
@@ -2,7 +2,7 @@
 #'
 #' Edit function definition in /R subfolder
 #'
-#' A convenience wrapper for \code{\link[usethis]{edit_file}} to edit or View a galacticPubs function. If in the galacticPubs dev project, it will edit; otherwise view.
+#' A convenience wrapper for \code{\link[usethis]{edit_file}} to edit or View a galacticPubs function. If in the galacticPubs dev project, it will edit; otherwise view. If you're in the galacticPubs dev project and name a nonexistent function, it'll prompt you if you want to create one.
 #'
 #' @param function_name name of a galacticPubs function
 #' @export
@@ -10,6 +10,10 @@
 efx <- function(function_name){
   isGalacticPubs<-grepl("galacticPubs",rstudioapi::getActiveProject(),fixed = TRUE)
   if(isGalacticPubs){
+    if(length(fs::dir_ls(fs::path("R"),type="file",regexp = substitute(function_name)))==0 ){
+      resp<-readline(paste0("Do you want to create a new function called ",substitute(function_name),"? (y/n) >"))
+      if(resp=="n"){stop()}
+    }
     usethis::edit_file((fs::path("R",substitute(function_name),ext="R")))
   }else{View(eval(parse(text=paste0("galacticPubs::",substitute(function_name)))))}
 }
