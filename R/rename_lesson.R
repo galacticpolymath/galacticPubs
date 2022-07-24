9060943908c7ebc0bb494375b32495a548104dff
Matt Wilkins <matt@galacticpolymath.com>
2022-07-24 17:44:40 -0500

    added ability to only change prefixed file and subfolder names

--- R/rename_lesson.R
+++ R/rename_lesson.R
@@ -9,10 +9,11 @@
 #'
 #' Assumes that you have Google Drive for Desktop set up with access to Lessons/ folder; github and gh CLI set up with proper permissions with GP GitHub. Will ignore case to account for different user behaviors.
 #' @param new_proj_name The new name you want to give the selected project
+#' @param new_ShortTitle New ShortTitles to be swapped out in lesson project file names. If blank, will try to guess by ignoring terminal "_suffixes"
 #' @param gh_proj_name The unique project title of this lesson which is prefixed on the lesson folder name and the GitHub project. Not *necessarily* the same as the ShortTitle used in naming lesson presentations and worksheets; probably more specific with underscores; If left off, will try to get this info from the GitHubPath if available in the front-matter.yml.
 #' @param curr_ShortTitle Current ShortTitle prefixed to lesson project files. If missing, will try to read this from ShortTitle in the existing front-matter.yml
-#' @param new_ShortTitle New ShortTitles to be swapped out in lesson project file names. If blank, will try to guess by ignoring terminal "_suffixes"
 #' @param lessons_dir path to the virtualized folder Edu/lessons, where all the lessons are found; default="/Volumes/GoogleDrive/My Drive/Edu/Lessons"
+#' @param only_rename_prefixes Do you want to only change project files with the ShortTitle at the beginning of the filename? (Could avoid accidental replacements if short title is a common phrase); default=TRUE
 #' @param change_this passed to [update_fm()] if you want to make any other changes to front matter. Must be a list of values to change in the front matter before rebuilding. Default=NULL. Example: list(Title="Stormy Misty's Foal") would change the title of the lesson to the name of a horsey book If gh_proj_name=="all", make sure you set this to something you want to change for everything.
 #' @param preserve_spaces if some files have a space in the 'Short Title', do you want to preserve this? default=FALSE
 #' @param run_check_wd logical; do you want to run [check_wd()]? Basically looks for files and folders you expect in a valid lesson project. default=TRUE
@@ -24,9 +25,9 @@ rename_lesson <- function(new_proj_name,
                           new_ShortTitle,
                           gh_proj_name,
                           curr_ShortTitle,
-                          lessons_dir,
-                          ignore_suffix = TRUE,
                           change_this = NULL,
+                          lessons_dir,
+                          only_rename_prefixes = TRUE,
                           preserve_spaces = FALSE,
                           run_check_wd = TRUE,
                           force_init_capital = TRUE) {
@@ -81,7 +82,6 @@ rename_lesson <- function(new_proj_name,
 
   #Enforce initial capital letter in all names if requested
   if(force_init_capital){
-
     new_ShortTitle<-string_capitalize_first(new_ShortTitle)
     new_proj_name <- string_capitalize_first(new_proj_name)
   }
@@ -103,6 +103,8 @@ message(
     curr_ShortTitle,
     "' to '",
     new_ShortTitle,
+    "\n    -only change prefixes: ",
+    only_rename_prefixes,
     "'?\n!!!\n"
   )
 )
@@ -122,6 +124,12 @@ if(test_folderRename){
 # 2. Find and rename all files & subfolders found in the project folder  --------
 #Deal with specific scenario where replacement is substring of current name
 newstr_is_substr<-grepl(new_ShortTitle,curr_ShortTitle)
+
+#Now add prefix to gsub pattern to just grab prefixes, if requested
+if(only_rename_prefixes){
+  curr_ShortTitle<-paste0("^",curr_ShortTitle)
+}
+
 #In this case, we need to rename things through an intermediate temp name
 
 # browser()
