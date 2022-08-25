f3846ec19dbfab20f7678a6d997fe142d412a054
Matt Wilkins <matt@galacticpolymath.com>
2022-08-25 11:58:20 -0500

    fixed pick_lesson() to deal with Apple/Google's rearrangement of where Google Drive virtual drive lives

--- R/pick_lesson.R
+++ R/pick_lesson.R
@@ -9,8 +9,29 @@
 
 pick_lesson<- function(lessons_dir,full_path=FALSE){
   if (missing(lessons_dir)) {
+    gdrive_dir<-  fs::path(fs::path_home(),"Library","CloudStorage")
+    if(!dir.exists(gdrive_dir)){
+      warning("path not found: ",gdrive_dir)
+      stop("Google Drive folder not found. You need Google Drive for Desktop to proceed.")
+    }
+   gdrive_userdir<- Sys.getenv("galacticPubs_gdrive_userdir")
+
+    if(is_empty(gdrive_userdir)){
+      gdrive_accounts<-basename(fs::dir_ls(gdrive_dir))
+      print(data.frame(Option=1:length(gdrive_accounts),Account=gdrive_accounts))
+      message("Set your Google Drive for Desktop user name with access to Edu/Lessons/:")
+      which_user<-as.numeric(readline("CHOICE: "))
+      Sys.setenv(galacticPubs_gdrive_userdir=gdrive_accounts[which_user])
+      message("Google Drive User saved for next time: ",gdrive_accounts[which_user])
+      gdrive_userdir<-Sys.getenv("galacticPubs_gdrive_userdir")
+      #need to manage this with functions...e.g. drive_user_set and drive_user_reset()
+    }
+
     lessons_dir <-
-      fs::path("/Volumes", "GoogleDrive", "My Drive", "Edu", "Lessons")
+      fs::path(fs::path_home(),"Library","CloudStorage",gdrive_userdir, "My Drive", "Edu", "Lessons")
+    if(!dir.exists(lessons_dir)){
+      stop("Something went wrong. Lessons Folder not found at:\n ",lessons_dir)
+    }
   }
   projects0<-fs::dir_ls(lessons_dir,type="directory")
   #Filter out some patterns for things we don't want to not process
