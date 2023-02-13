74b12ef07604ef4be3b8f08bfde9c6200ba56715
Matt Wilkins <matt@galacticpolymath.com>
2023-02-12 22:05:45 -0600

    init_lesson_meta working! Copies google drive meta template files to project meta/ folder

--- R/drive_new_from_template.R
+++ R/drive_new_from_template.R
@@ -1,10 +1,12 @@
 #' drive_new_from_template()
 #'
-#' Create a new Galactic Polymath Google Drive file from a template
+#' Create a new Galactic Polymath Google Drive file from a template file.
+#'
+#' If you're wanting to copy meta/ google sheets, check out [init_lesson_meta()]
 #'
 #' @param template_path text string of a path to a template to pass to [drive_find_path()]
 #' @param dest_path text string of a path where you want to file to go
-#' @param new_name text string of what you'd like to name your new file. default=NULL results in "Copy of FILENAME"; passed to [googledrive::drive_cp()]; you can also provide FALSE to
+#' @param new_name text string of what you'd like to name your new file. default=FALSE maintains the original name; NULL results in "Copy of FILENAME"; passed to [googledrive::drive_cp()]
 #' @param overwrite Do you want to overwrite an existing file? default=NA means overwrite. Other options T or F are very inefficient; passed to [googledrive::drive_cp()]
 #' @param ... pass other arguments to Google Drive API (see [googledrive::drive_cp()])
 #' @family Google Drive Functions
@@ -14,29 +16,49 @@
 drive_new_from_template <-
   function(template_path = NULL,
            dest_path = NULL,
-           new_name = NULL,
+           new_name = FALSE,
            overwrite = NA,
            ...) {
     if (is.null(template_path) | is.null(dest_path)) {
       stop("Must supply a template and destination path to pass to drive_find_path")
     }
 
-    from_path <-
-      drive_find_path(template_path) %>% catch_err(keep_results = TRUE)
-    to_path <-
-      drive_find_path(dest_path) %>% catch_err(keep_results = TRUE)
-
-    if (from_path$success & to_path$success) {
-      if (!is.null(new_name) & identical(new_name, FALSE)) {
-        # enforce old name with no prefix if user supplied new_name=F
-        new_name <- from_path$result$name
+    if (!inherits(template_path, "dribble")) {
+      #if a drive ID supplied, wrapped in as_id(), we need to get it, so everything is a dribble going forward
+      #we need to know the name of the file to prevent "copy of filename" naming behavior
+      is_template_drive_id <-
+        checkmate::check_class(template_path, "drive_id")
+      if (is_template_drive_id) {
+        from_path <- googledrive::drive_get(id = template_path)
+      } else{
+        from_path <-
+          drive_find_path(template_path) %>% catch_err(keep_results = TRUE) %>% .result
       }
+      #don't need to do anything if it's already a dribble
+    }else{from_path<-template_path}
+    #from here template_path should always be a dribble of at least one row
 
-      googledrive::drive_cp(from_path$result,
-                            to_path$result,
-                            name = new_name,
-                            overwrite = overwrite)
+    is_dest_drive_id <- checkmate::check_class(dest_path, "drive_id")
+    if (is_dest_drive_id) {
+      to_path <- googledrive::drive_get(id = dest_path)
     } else{
-      warning("Problem with template or destination path")
+      to_path <-
+        drive_find_path(dest_path) %>% catch_err(keep_results = TRUE) %>% .result
     }
+
+    #if False supplied for new_name, use same name as from_path (no copy of...nonsense)
+    if (!is.null(new_name) & identical(new_name, FALSE)) {
+      # enforce old name with no prefix if user supplied new_name=F
+      new_name <- from_path$name
+    }
+
+    out<-pbapply::pblapply(1:nrow(from_path), function(i) {
+      googledrive::drive_cp(from_path[i,],
+                          to_path,
+                          name = new_name[i],
+                          overwrite = overwrite)
+    })
+    out %>% dplyr::bind_rows()
+
+
   }
--- R/init_lesson_meta.R
+++ R/init_lesson_meta.R
@@ -5,24 +5,32 @@
 #' WD is used for hybrid file path navigation using virtualized paths on Google Drive for desktop to reach the Edu/Lessons/meta/front-matter.yml for the project of interest. Then, using [get_fm()] to lookup the GoogleDrive ID for the cloud version of this folder, files will then be copied from the template folder to this project folder using the google web API. All of this is necessary because Google Drive for Desktop doesn't allow copying of Google Documents.
 #'
 #' @param WD the working directory for the virtualized lesson path; default=getwd() if you're working in the lesson's .Rproj. Otherwise use [pick_lesson()]
+#' @param overwrite passed to [googledrive::drive_cp()]; default=NA will add "(1)" to avoid duplicates; TRUE will trash existing file before copying
 #' @family Google Drive Functions
 #' @export
 
-init_lesson_meta <- function(WD=getwd()){
+init_lesson_meta <- function(WD=getwd(),overwrite=NA){
 
   #check_wd(WD)
-
   #GdriveID for lesson templates (must have access to '/GP-Workshop/Templates_BE_CAREFUL/lesson-meta-templates/')
-  gID<-get_fm(,WD=WD)
-  drive_new_from_template(template_path = "1Faa1RCf6zRbvIn1ek6jLsvp3nOip12me",dest_path = )
+  dest_gID<-get_fm("GdriveMetaID",WD=WD) %>% unlist
+
+  checkmate::assert(
+    checkmate::check_class(dest_gID,"character")
+  )
+
+  meta_template_files<-googledrive::drive_get(id=googledrive::as_id("1Faa1RCf6zRbvIn1ek6jLsvp3nOip12me")) %>% drive_contents
+  drive_new_from_template(meta_template_files,googledrive::as_id(dest_gID),overwrite=overwrite)
+  # drive_new_from_template(template_path = "1Faa1RCf6zRbvIn1ek6jLsvp3nOip12me",dest_path = dest_gID)
 
 
 }
 
 
-#' lesson_init_meta()
+#' lesson_init_meta
 #'
 #' @describeIn init_lesson_meta
+#'
 #' @export
 #'
 lesson_init_meta <- init_lesson_meta
