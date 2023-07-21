0f1a021722b8d49208be35a84311ee4479451fe4
Matt Wilkins <matt@galacticpolymath.com>
2023-07-21 18:40:57 -0500

    lesson_init now creates directories for a new project. Next to copy templates

--- R/lesson_init.R
+++ R/lesson_init.R
@@ -9,8 +9,86 @@
 #'
 
 lesson_init <- \(ShortName){
+
+
+# Get details for new lesson with helper shiny app ------------------------
 inputs <- lesson_init_helper()
-print(inputs)
+checkmate::assert_list(inputs,all.missing=FALSE)
+
+#add locale to inputs
+inputs2 <- parse_locale(inputs)
+unit_name <- paste0(c(inputs$ShortName,inputs2$locale),collapse="_")
+
+# Check if ShortName is valid and not a duplicate -------------------------
+checkmate::assert_character(inputs$ShortName,min.chars = 2,max.chars=10,all.missing = FALSE,len = 1)
+
+#location of gp-lessons git project (not a google drive folder)
+gp_lessons_dir <- fs::path(get_git_gp_lessons_path(),"Lessons")
+checkmate::assert_directory_exists(gp_lessons_dir,access="w")
+
+#location of GP-Studio Google Shared Drive /Edu/Lessons folder (where proj will go)
+studio_lessons_dir <- get_lessons_path("s")
+checkmate::assert_directory_exists(studio_lessons_dir,access="w",.var.name ="GP-Studio/Edu/Lessons" )
+
+#Test for name redundancy
+existing_units <- list.files(gp_lessons_dir)
+is_unique <- !unit_name %in% existing_units
+checkmate::assert_true(is_unique,.var.name="Unique Unit/lesson name. Cannot match existing 'gp-lessons' project.")
+
+#Test that this project doesn't already exist on GP-Studio
+new_dir <- fs::path(studio_lessons_dir,unit_name)
+new_dir_exists <- checkmate::test_directory_exists(new_dir)
+
+checkmate::assert_false(new_dir_exists,.var.name="New directory doesn't already exist on GP-Studio")
+
+
+# Create new project directory and subfolders --------------------------------------------
+#Define Subfolder structure we always want to see!
+### assets subfolders
+asset_subdir <-
+  c("_banners_logos_etc",
+      "_learning_plots",
+      "_orig-client-media_NoEdit",
+      "_other-media-to-publish",
+      "_R_outputs",
+      "_videos-for-this-lesson")
+asset_dirs <- c(fs::path(new_dir,"assets",asset_subdir))
+
+### teaching-materials subfolders
+teach_mat_suffix <- paste_valid(
+    tolower(inputs$GradesOrYears),
+    paste0(inputs$min_grade, "-", inputs$max_grade)
+  )
+teach_mat_envir_dirs <- sapply(inputs$LessonEnvir,\(x){paste_valid(x,teach_mat_suffix)})
+
+teach_mat_dir <- fs::path(new_dir, "teaching-material", teach_mat_envir_dirs )
+
+#Add Subfolders with Lx if we've specified more than 1 lesson in this unit
+if(inputs$n_lessons>1){
+#everything will write recursively, so we only have to specify the most specific paths,
+#intermediate folders will be created automatically
+  teach_dirs <- sapply(1:inputs$n_lessons,\(i){fs::path(teach_mat_dir,paste0("L",i))})
+}else{
+  teach_dirs <- teach_mat_dir
+}
+
+### Other folders to create in the root project folder
+other_names <- c("reading-material",
+                   fs::path("data","_orig-client-data_NoEdit"))
+other_dirs <- fs::path(new_dir,other_names)
+
+
+# Now Create all subfolders -----------------------------------------------
+all_paths <- c(asset_dirs,teach_dirs,other_dirs)
+new_dir_success <- fs::dir_create(all_paths,recurse=TRUE) %>% catch_err()
+
+#Always create the teaching materials folder
+if(inputs$bool_teach){
+
+}else{
+  teaching_mat_dirs <- NULL
+}
+
 }
 
 #' init_lesson
--- inst/shiny/lesson_init_helper/app.R
+++ inst/shiny/lesson_init_helper/app.R
@@ -6,47 +6,118 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    titlePanel("Initialize a new Galactic Polymath Mini-Unit"),
-    textInput("ShortName",label = "ShortName prefix for project",value = "",width = 200),
-    selectizeInput(
-            inputId = "Language",
-            label = "LESSON\nLanguage",
-            choices = c("",language_codes$Name),
-            selected = "English",
-            options = list(create = TRUE),
-            width ="150"
-          ),
-          selectizeInput(
-            inputId = "Country",
-            label = "LESSON Country (can leave blank)",
-            choices = c("",country_codes$Name),
-            selected = "United States",
-            options = list(create = TRUE),
-            width ="200"
-          ),
-    numericInput("n_lessons","Number of Lessons",value=0,min=0,max=10,step=1,width="150"),
-    checkboxInput("bool_init_meta",label = "Copy templates to meta/ folder?",value = TRUE,width = "200"),
-    checkboxInput("bool_pres",label= "Copy Presentation template to teaching-materials/L# folder(s)?",value=TRUE,width="200"),
-    checkboxInput("bool_teach",label= "Copy Teacher Worksheet template to teaching-materials/L# folder(s)?",value=TRUE,width="200"),
-    actionButton("submit","Make it so!")
+  titlePanel("Initialize a new Galactic Polymath Mini-Unit"),
+  textInput(
+    "ShortName",
+    label = "ShortName prefix for project",
+    value = "",
+    width = 200
+  ),
+  selectizeInput(
+    inputId = "Language",
+    label = "LESSON\nLanguage",
+    choices = c("", language_codes$Name),
+    selected = "English",
+    options = list(create = TRUE),
+    width = "150"
+  ),
+  selectizeInput(
+    inputId = "Country",
+    label = "LESSON Country (can leave blank)",
+    choices = c("", country_codes$Name),
+    selected = "United States",
+    options = list(create = TRUE),
+    width = "200"
+  ),
+  div(style="inline-block",
+  selectizeInput(
+    inputId = "min_grade",
+    label = "Min Grade",
+    choices= c("",5:12, "university"),
+    selected = NULL,
+    width= 100
+  ),
+  selectizeInput(
+    inputId = "max_grade",
+    label = "Max Grade",
+    choices= c("",5:12, "university"),
+    selected = NULL,
+    width= 100
+  )
+  ),
 
-)
+  selectizeInput(
+    inputId = "GradesOrYears",
+    label = "Word for Grades",
+    choices = c("Grades","Years"),
+    selected = "Grades",
+    options = list(create= TRUE),
+    width= "200"
+  ),
+  checkboxGroupInput(
+    "LessonEnvir",
+    "Envir",
+    choices = c(
+      "classroom" = "classroom",
+      "remote" = "remote"),
+      selected = "classroom",
+      inline = T,
+      width = 250
+    ),
+    numericInput(
+      "n_lessons",
+      "Number of Lessons",
+      value = 0,
+      min = 0,
+      max = 10,
+      step = 1,
+      width = "150"
+    ),
+    checkboxInput(
+      "bool_init_meta",
+      label = "Copy templates to meta/ folder?",
+      value = TRUE,
+      width = "200"
+    ),
+    checkboxInput(
+      "bool_pres",
+      label = "Copy Presentation template to teaching-materials/L# folder(s)?",
+      value = TRUE,
+      width = "200"
+    ),
+    checkboxInput(
+      "bool_teach",
+      label = "Copy Teacher Worksheet template to teaching-materials/L# folder(s)?",
+      value = TRUE,
+      width = "200"
+    ),
+    actionButton("submit", "Make it so!"),
+  div(style="margin-bottom: 15px;")
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+  )
 
-    observe(
-      isolate({
+  # Define server logic required to draw a histogram
+  server <- function(input, output) {
+    observe(isolate({
       # .GlobalEnv$galacticPubs_setLanguage<-input$Language
       # .GlobalEnv$galacticPubs_setCountry<-input$Country
       result <- list(
-        Language=input$Language,
-        Country=input$Country
+        ShortName = input$ShortName,
+        Language = input$Language,
+        Country = input$Country,
+        min_grade = input$min_grade,
+        max_grade = input$max_grade,
+        GradesOrYears = input$GradesOrYears,
+        LessonEnvir = input$LessonEnvir,
+        n_lessons = input$n_lessons,
+        bool_init_meta = input$bool_init_meta,
+        bool_pres = input$bool_pres,
+        bool_teach = input$bool_teach
+
       )
       stopApp(result)
-      })
-    ) %>% bindEvent(input$submit)
-}
+    })) %>% bindEvent(input$submit)
+  }
 
-# Run the application
-shinyApp(ui = ui, server = server)
+  # Run the application
+  shinyApp(ui = ui, server = server)
