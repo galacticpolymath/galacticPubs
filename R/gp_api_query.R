beba528efcdd5fd65cdb4cd32ade0afd15bcf445
Matt Wilkins <matt@galacticpolymath.com>
2023-09-26 20:27:53 -0500

    delete through API works!

--- R/gp_api_query.R
+++ R/gp_api_query.R
@@ -13,16 +13,16 @@
 gp_api_query <- \(
   keys = NULL,
   numID = NULL,
-  id=NULL,
+  id = NULL,
   output_tibble = TRUE
 ) {
-  if(!is.null(numID)&!is.null(id)){
+  if (!is.null(numID) & !is.null(id)) {
     stop("Only supply numID OR _id.")
   }
   tictoc::tic()
 
   #return basic results for all units if no keys and no ids provided
-  if(is.null(keys)&is.null(numID)&is.null(id)){
+  if (is.null(keys) & is.null(numID) & is.null(id)) {
     keys <- "basic"
   }
 
@@ -32,35 +32,39 @@ gp_api_query <- \(
     httr2::request("https://dev.galacticpolymath.com//api/get-lessons")
 
   #Add filterObj to query to filter by numID and `_id`
-  if (!is.null(id)|!is.null(numID)) {
-    if(!is.null(numID)){
-      filterList <- list(numID=numID)
-    }else{
-      filterList <- list(`_id`=id)
-      }
+  if (!is.null(id) | !is.null(numID)) {
+    if (!is.null(numID)) {
+      filterList <- list(numID = numID)
+    } else{
+      filterList <- list(`_id` = id)
+    }
 
-      req <- req0 %>% req_url_query(filterObj =
-                                     paste0(as.character(
-                                       jsonlite::toJSON(filterList, auto_unbox = FALSE)
-                                     )))
-    }else{req <- req0}
+    req <- req0 %>% httr2::req_url_query(filterObj =
+                                           paste0(as.character(
+                                             jsonlite::toJSON(filterList, auto_unbox = FALSE)
+                                           )))
+  } else{
+    req <- req0
+  }
 
   #Combine filter with projections to make final query
-  if(!is.null(keys)){
-     if(identical(keys,"basic")){keys <- c("numID", "_id", "Title")}
+  if (!is.null(keys)) {
+    if (identical(keys, "basic")) {
+      keys <- c("numID", "_id", "Title", "LastUpdated")
+    }
     #this is dumb b/c these keys don't match Lesson.json hierarchy
     # lapply(keys, \(x) checkmate::assert_choice(x, get_fm_names()))
 
 
-  params <- lapply(keys, \(x) {
-    1
-  })
-  names(params) <- keys
-  req_final <- req %>% httr2::req_url_query (projectionsObj =
-                          paste0(as.character(
-                            jsonlite::toJSON(params, auto_unbox = TRUE)
-                          )))
-  }else{
+    params <- lapply(keys, \(x) {
+      1
+    })
+    names(params) <- keys
+    req_final <- req %>% httr2::req_url_query (projectionsObj =
+                                                 paste0(as.character(
+                                                   jsonlite::toJSON(params, auto_unbox = TRUE)
+                                                 )))
+  } else{
     req_final <- req
   }
 
@@ -70,18 +74,28 @@ gp_api_query <- \(
   #for printout
   req_final %>% httr2::req_dry_run()
   #actually run request
-  res <- req_final %>% httr2::req_perform()
-  out <- res %>%
-    httr2::resp_body_json() %>% .[[1]]
+  res <-
+    req_final %>% httr2::req_perform() %>% catch_err(keep_results = TRUE)
 
 
+    out <- res$result %>%
+      httr2::resp_body_json() %>% .[[1]]
 
-  #This will silently leave out columns if they don't fit into a tibble :/
-  if (output_tibble) {
-    out2 <-
-      out %>% tidyjson::as_tbl_json() %>%  tidyjson::spread_all() %>%
-      dplyr::arrange(dplyr::desc(.data$`_id`))
-  }else{out2 <- out}
+  if (length(out)>0) {
+
+    #This will silently leave out columns if they don't fit into a tibble :/
+    if (output_tibble) {
+      out2 <-
+        out %>% tidyjson::as_tbl_json() %>%  tidyjson::spread_all() %>%
+        dplyr::arrange(dplyr::desc(.data$`_id`)) %>%
+        dplyr::relocate(keys)
+    } else{
+      out2 <- out
+    }
+  } else{
+    message("No records found for this unit.")
+    out2 <- NULL
+  }
   tictoc::toc()
   out2
 
--- R/gp_api_unit_delete.R
+++ R/gp_api_unit_delete.R
@@ -1,16 +1,63 @@
-#' Delete a GP unit
+#' Delete a GP unit from the database
 #'
-#' Deletes a unit from MongoDB using the GP API
+#' Deletes a unit from MongoDB using the GP API. Does not delete it from Google Drive or GitHub gp-lessons project.
 #'
 #' @param WD working directory, passed to [parse_wd()];default="?"
+#' @param unit_id instead of supplying WD, you can supply the _id directly for the unit that you want to delete.
 #' @family GP API
 #' @export
 #' @returns success (logical)
 #'
 
-gp_api_unit_delete <- \(WD="?"){
-
+gp_api_unit_delete <- \(WD = "?", unit_id = NULL) {
   token <- get_gp_api_token()
+  if (is.null(unit_id)) {
+    WD <- parse_wd(WD)
+    unit_id <- get_fm("_id", WD)
+  }
 
+  checkmate::assert_character(unit_id, all.missing = FALSE, min.chars =
+                                5)
+
+  # test if this unit exists in GP-Catalog ----------------------------------
+  unit_missing <-
+    gp_api_query(id = unit_id, c("Title", "Subtitle")) %>% is_empty()
+
+  if (unit_missing) {
+    message("No unit found on GP-Catalog with _id == '", unit_id, "'.")
+    test_request <- test_delete <- FALSE
+  } else{
+    req <-
+      httr2::request("https://dev.galacticpolymath.com/api/delete-lesson") %>%
+      httr2::req_auth_bearer_token(token = token) %>%
+      httr2::req_method("DELETE") %>%
+      httr2::req_url_query(`_id` = unit_id)
+
+    httr2::req_dry_run(req)
+    message(
+      "\n***********************************\n",
+      " Are you sure you want to delete mini-unit '",
+      unit_id,
+      "' from the GP-Catalog?"
+    )
+    choice <- readline("(y/n)? >")
+    if (choice != "y") {
+      stop("Unit deletion aborted.")
+    }
+
+
+    test_request <-
+      httr2::req_perform(req, verbosity = 3) %>% catch_err()
+
+    test_delete <-
+      gp_api_query(id = unit_id, c("Title", "Subtitle")) %>% is_empty()
+  }
+  if (test_request & test_delete) {
+    message("Deletion SUCCEEDED for '", unit_id, "'.")
+    TRUE
+  } else{
+    message("Deletion FAILED for '", unit_id, "'.")
+    FALSE
+  }
 
 }
