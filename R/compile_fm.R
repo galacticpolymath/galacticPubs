e6d7eab71956e623f7fc8b4d88a20bf66fba684e
Matt Wilkins <matt@galacticpolymath.com>
2025-07-08 16:28:59 -0500

    make versions get sorted

--- R/compile_fm.R
+++ R/compile_fm.R
@@ -119,8 +119,10 @@ compile_fm <- \(WD = "?") {
   save_json(header, filename = fs::path(json_dir, "header", ext = "json"))
 
   # versions.json -----------------------------------------------------------
+
   ver <-
-    get_fm("Versions", WD = WD, standardize_NA = FALSE)[[1]] %>% dplyr::as_tibble()
+    get_fm("Versions", WD = WD, standardize_NA = FALSE)[[1]] %>%
+    dplyr::as_tibble() %>% dplyr::arrange(.data$ver_num)#ensure latest entry always at bottom
 
   if (is_empty(ver)) {
     ver_out0 <- NULL
