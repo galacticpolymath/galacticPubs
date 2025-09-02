993a7345b469a165750fe53f1051e66153383198
Matt Wilkins <matt@galacticpolymath.com>
2025-09-02 17:37:06 -0500

    first successful transfer of teaching-mat to galacticpolymath/ with new logic
    Still to do:
    1. Work on logic for partial transfer to teaching-materials_DEV (for FutureFoods)
    2. Make reverse function for make_teaching_mat_private
    3. Incorporate into "organize_teach_it()" in compile_teach_it()

--- R/make_teaching_mat_public.R
+++ R/make_teaching_mat_public.R
@@ -10,20 +10,35 @@
 #'    - GdriveTeachMatPath: 'GalacticPolymath/MediumTitle'
 #'
 #' @param WD is working directory of the project; easiest way to supply a different lesson is with "?", which will invoke [pick_lesson()]; default is WD=getwd()
+#' @param uinfo is a data frame of unit info, typically read from the "Titles" tab of the teach-it_*.gsheet; default is NULL, which will cause the function to read it in
+#' @return A logical TRUE/FALSE indicating overall success. NA if no action was needed (i.e. teaching-materials already at GalacticPolymath)
 #' @export
 
-make_teaching_mat_public <- \(WD = "?") {
+make_teaching_mat_public <- \(WD = "?", uinfo = NULL) {
   WD <- parse_wd(WD)
-
+  #authenticate with default email for this user
+  oauth_email <- Sys.getenv("galacticPubs_gdrive_user")
+  checkmate::assert_string(oauth_email, .var.name = "galacticPubs_gdrive_user")
+  googledrive::drive_auth(email = oauth_email)
+  googlesheets4::gs4_auth(email = oauth_email)
+  #Check if anything needs to be done (if teaching-materials are not already at GalacticPolymath)
+  gdrive_teach_mat_curr_path <- get_fm("GdriveTeachMatPath", WD)
+  if (grepl("^GalacticPolymath", gdrive_teach_mat_curr_path)) {
+    message(
+      "make_teaching_mat_public(): Teaching materials already at GalacticPolymath! Nothing to do."
+    )
+    return(NA)
+  }
 
   # Extract important front-matter  -----------------------------------------
   newTitle <- get_fm("MediumTitle", WD)
   dirID <- get_fm("GdriveDirID", WD)
   projDirName <- get_fm("GdriveDirName", WD)
   gpID <- get_fm("GdrivePublicID", WD)
-  tmID <- get_fm("GdrivePublicID", WD)
+  tmID <- get_fm("GdrivePublicID", WD) #/teaching-materials folder Google Drive ID
   dir_drib <- drive_find_path(dirID)
-  teachitID <- get_fm("GdriveTeachItID", WD)
+  teachitID <- get_fm("GdriveTeachItID", WD) #ID of the teach-it_*.gsheet
+  teachMatDevID <- get_fm("GdriveTeachMatDevID", WD) #ID of the /teaching-materials_DEV/ folder (may not exist)
 
   checkmate::assert_character(
     newTitle,
@@ -44,6 +59,13 @@ make_teaching_mat_public <- \(WD = "?") {
     min.chars = 6,
     .var.name = "teaching material google ID (GdrivePublicID)"
   )
+
+  checkmate::assert_character(
+    teachMatDevID,
+    null.ok = F,
+    all.missing = TRUE,
+    .var.name = "teaching material DEV folder google ID (teachMatDevID)"
+  )
   # test_not_published <- checkmate::test_scalar_na(gpID)
   # checkmate::assert_data_frame(
   #   dir_drib,
@@ -53,41 +75,139 @@ make_teaching_mat_public <- \(WD = "?") {
   # )
 
 
-  if (is.null(teach_it_drib)) {
-    teachitID <- fm$GdriveTeachItID
-    checkmate::assert_character(teachitID, all.missing = FALSE)
-    teach_it_drib <- drive_find_path(teachitID)
-  }
-  #check that teach_it_drib has 1 row and is a data frame
-  checkmate::assert_data_frame(teach_it_drib, nrows = 1)
+  # if (is.null(teach_it_drib)) {
 
-  # #If a publicID (on GalacticPolymath) has been assigned, we can skip the moving step
-  # if (!test_not_published) {
-  #   message("A GdrivePublicID already found. Skipping move to GP-LIVE and GalacticPolymath'")
-  #   live_success <-
-  #     gp_success <-
-  #     shortcut_success <-
-  #     made_public_success <-
-  #     test_fm1 <- test_fm2 <- update_success <-  NA
-  # } else{
-  #only try to look up teaching-materials in unpublished projects
+
+
+  # Read in unit info tab of teaching-materials.gsheet ----------------------
+  # Get lesson statuses
+  if (is.null(uinfo)) {
+    uinfo <-
+      googlesheets4::read_sheet(
+        googledrive::as_id(teachitID),
+        sheet = "Titles",
+        skip = 1,
+        col_types = "c"
+      )
+  }
+  checkmate::assert_data_frame(uinfo, min.rows = 0, .var.name = "teach-it.gsheet!Titles")
+
+  if (nrow(uinfo) == 0) {
+    message(
+      "make_teaching_mat_public(): No lsnStatuses found on 'teach-it.gsheet!Titles'! Cannot proceed."
+    )
+    warning(
+      "make_teaching_mat_public() failed for project '",
+      projDirName,
+      "' b/c no lessons found on 'teach-it.gsheet!Titles'"
+    )
+    return(FALSE)
+  }
+
+
+  # Get teaching materials dribble --------------------------------------------------
   tm_drib <-
     drive_find_path(tmID, drive_root = dir_drib)
   checkmate::assert_data_frame(tm_drib, all.missing = FALSE, .var.name = "'/teaching-materials/' Google Drive object (dribble)")
   checkmate::assert(googledrive::is_folder(tm_drib), .var.name = "/teaching-materials/ Google Drive object (dribble)")
 
+  if (!is_empty(teachMatDevID)) {
+    tm_dev_drib <-
+      drive_find_path(teachMatDevID, drive_root = dir_drib)
+    checkmate::assert_data_frame(tm_dev_drib,
+                                 all.missing = TRUE,
+                                 .var.name = "'/teaching-materials_DEV/' Google Drive object (dribble)")
+    checkmate::assert(googledrive::is_folder(tm_dev_drib), .var.name = "/teaching-materials_DEV/ Google Drive object (dribble)")
+  } else{
+    tm_dev_drib <- NULL
+    tm_dev_move_success  <- NA
+  }
 
 
-  # Prompt user before moving to GP-LIVE ------------------------------------
+  # Figure out tasks to be done ---------------------------------------------
 
+  # live lessons
+  live_lsns <- uinfo %>% dplyr::filter(.data$lsnStatus == "Live")
+
+  print(uinfo %>% dplyr::select("lsn", "lsnStatus", "lsnTitle"))
+
+  #If no live lessons, nothing to do
+  if (nrow(live_lsns) == 0) {
+    message(
+      "make_teaching_mat_public(): No 'Live' lessons found on 'teach-it.gsheet!Titles'! Cannot proceed."
+    )
+    warning(
+      "make_teaching_mat_public() failed for project '",
+      projDirName,
+      "' b/c no 'Live' lessons found on 'teach-it.gsheet!Titles'"
+    )
+    return(FALSE)
+  }
+
+  # nonlive lessons
+  nonlive_lsns <- uinfo %>% dplyr::filter(.data$lsnStatus != "Live")
+
+  #Check for actual non-live lessons in teaching-materials/ folder
+  if (nrow(nonlive_lsns) > 0) {
+    tm_drib_contents <-
+      googledrive::drive_ls(
+        tm_drib,
+        type = "folder",
+        recursive = TRUE,
+        n_max = Inf,
+        pattern = "^L"
+      ) %>%
+      dplyr::mutate(lsn = gsub("^L(\\d+).*", "\\1", .data$name))
+    #which (if any nonlive lessons need to be moved to /teaching-materials_DEV/?)
+    real_nonlive_lsns <- dplyr::inner_join(nonlive_lsns, tm_drib_contents, by =
+                                             "lsn")
+  } else{
+    real_nonlive_lsns <- data.frame()
+  }
+
+
+
+
+
+  # Check for existence of /teaching-materials_DEV/ folder if needed
+  if (nrow(real_nonlive_lsns) > 0 & is.null(tm_dev_drib)) {
+    message(
+      "make_teaching_mat_public(): Non-live lessons found on 'teach-it.gsheet!Titles' but no /teaching-materials_DEV/ folder found! Need to create it."
+    )
+    must_create_TM_dev <- TRUE
+  } else{
+    must_create_TM_dev <- FALSE
+  }
+
+
+
+  # Prompt user before moving teaching-materials/ ---------------------------
+  #Rename location for teaching-materials on the GalacticPolymath drive
+  tmPath <-
+    fs::path("GalacticPolymath", newTitle)
+  message(
+    "make_teaching_mat_public(): \n-------------------\nARE YOU SURE you want to make Teaching-Materials public?: ",
+    basename(WD),
+    " and:"
+  )
+  #Enumerate tasks for user to verify
+  # move nonlive (proto, hidden, upcoming) lessons to /teaching-materials_DEV/
+  if (nrow(real_nonlive_lsns) > 0) {
+    message(
+      " * move non-'live' Lesson(s) ",
+      paste(real_nonlive_lsns$lsn, collapse = ", "),
+      " to /teaching-materials_DEV/*"
+    )
+  }
 
   message(
-    "make_teaching_mat_public(): \n-------------------\nARE YOU SURE you want to:\n make Teaching-Materials public?: ",
-    basename(WD)
+    " * move 'live' Lesson(s) ",
+    paste(live_lsns$lsn, collapse = ", "),
+    "  in /teaching-materials/* to GalacticPolymath/ shared drive"
   )
-  message(" 1. move /teaching-materials/* to GalacticPolymath/ shared drive")
-  message(" 2. rename /teaching-materials to '", newTitle, "'")
-  message("**** This will remove almost everyone's edit access ****")
+  message(" * rename GalacticPolymath/teaching-materials to ",
+          newTitle)
+  message(" NOTE: This will remove almost everyone's edit access ****")
   continue <- readline("(y/n) > ")
 
   if (continue != "y") {
@@ -151,80 +271,42 @@ make_teaching_mat_public <- \(WD = "?") {
   # checkmate::assert_directory_exists(tmPath_full) %>% catch_err(try_harder = T, waits =
   #                                                            c(2, 5, 1, 9))
 
-  #Rename location for teaching-materials on the GalacticPolymath drive
-  tmPath <-
-    fs::path("GalacticPolymath", newTitle)
 
-  test_fm1 <- update_fm(WD = WD,
-                        change_this = list(GdriveTeachMatPath = tmPath)) %>% catch_err()
+  test_fm <- update_fm(WD = WD,
+                       change_this = list(GdriveTeachMatPath = tmPath)) %>% catch_err()
 
 
-  # if (!is.na(live_success) & live_success) {
-  #   gpID <- as.character(test_move_to_gp$result$from$id)
-  #   update_fm(
-  #     WD = WD,
-  #     change_this = list(
-  #       GdrivePublicID = unname(gpID),
-  #       GdrivePublicID = NA
-  #     )
-  #   )
-  #   test_fm2 <-
-  #     checkmate::test_character(get_fm("GdrivePublicID", WD = WD), all.missing = FALSE)
-  # } else if (!is.na(live_success) & !live_success) {
-  #   test_fm2 <- FALSE
-  # } else{
-  #   test_fm2 <- NA
-  # }
-
-  # Run compile_unit() to affect new locations of files -------------------
-
-  # if (live_success &
-  #     gp_success &
-  #     shortcut_success &
-  #     made_public_success & test_fm1 & test_fm2) {
-  #   message("Running compile_unit() to make sure the lesson is up-to-date.")
-  #   update_success <- compile_unit(WD = WD) %>% catch_err()
-  # } else{
-  #   message("Skipping compile_unit() b/c of step failures. Run manually if necessary.")
-  #   update_success <- FALSE
-  # }
-
-
-  # }
-  # }
 
   # Summarize results -------------------------------------------------------
   successes <-
-    c(
-      # live_success,
+    c(tm_dev_move_success,
       gp_success,
       shortcut_success,
       made_public_success,
-      test_fm1,
-      # test_fm2,
-      # update_success
-    ) %>% convert_T_to_check()
-  out <- dplyr::tibble(
-    success = successes,
+      test_fm)
+
+  SUCCESS <- all(successes,na.rm=TRUE)
+  if (SUCCESS) {
+    message("make_teaching_mat_public() SUCCESS for project '",
+            projDirName,
+            "'")
+  } else{
+    warning("make_teaching_mat_public() had ERRORS for project '",
+            projDirName,
+            "'")
+  }
+
+  out <- data.frame(
+    success = convert_T_to_check(successes),
     task = c(
-      # "move project to GP-LIVE",
+      "move non-live lessons to /teaching-materials_DEV/",
       "move /teaching-materials/ to GalacticPolymath",
       "create shortcut to moved /teaching-materials/",
-      "make teaching-materials public"
-      # paste0(
-      #   "update_fm(): GdriveHome='GP-LIVE' and PublicationStatus='Live' and GdriveTeachmatPath=",
-      #   tmPath
-      # ),
-      # paste0(
-      #   "update_fm(): GdrivePublicID='",
-      #   gpID,
-      #   "' and GdrivePublicID= NA"
-      # ),
-      # "compile_unit()"
+      "make teaching-materials public",
+      "update front-matter (GdriveTeachMatPath)"
     )
   )
 
-  message("make_teaching_mat_public() summary:")
-  print(out)
-  return(all(success))
+  print(format(out, justify = "left"), row.names = FALSE)
+  return(SUCCESS)
 }
